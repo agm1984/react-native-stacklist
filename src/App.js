9ebea02f88b947a94108144990a7e3cb4ef2447a
Adam Mackintosh <agm1984@gmail.com>
2017-10-03 17:15:49 -0700

    added common components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+
+// Provider: works with store, to translate data in store          (the glue)
+// for use by the React Application (communication with React)
+// Pass the combined reducers in to createStore
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
+    )
+}
+
+export default App
